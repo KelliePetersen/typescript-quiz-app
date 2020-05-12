00c3cce2835b2bad80d06562e13ac4e18e18e962
KelliePetersen <kelliepetersen@outlook.com>
2020-05-12 10:00:00 -0600

    add logic to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import QuestionCard from './components/QuestionCard';
 
 function App() {
+  const startQuiz = async () => {
+
+  }
+
+  const checkAnswer = (event: React.MouseEvent<HTMLButtonElement>) => {
+
+  }
+
+  const nextQuestion = () => {
+
+  }
+
   return (
     <div className="App">
-      Quiz
+      <h1>REACT QUIZ</h1>
+      <button className="start" onClick={startQuiz}>
+        Start Quiz
+      </button>
+      <p className="score">Score:</p>
+      <p>Loading questions...</p>
+      <QuestionCard />
+      <button className="next" onClick={nextQuestion}>
+        Next
+      </button>
     </div>
   );
 }
