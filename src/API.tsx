cfc5bd71f12ea7a1003861bca9f17320de759151
KelliePetersen <kelliepetersen@outlook.com>
2020-05-13 10:00:00 -0600

    add types logic to API and include in App

--- src/API.tsx
+++ src/API.tsx
@@ -1,3 +1,15 @@
+import { shuffleArray } from './utils'
+
+export type Question = {
+  category: string;
+  correct_answer: string;
+  difficulty: string;
+  incorrect_answers: string[];
+  question: string;
+  type: string;
+}
+
+export type QuestionState = Question & { answers: string[] };
 
 export enum Difficulty {
   EASY = "easy",
@@ -8,5 +20,10 @@ export enum Difficulty {
 export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty) => {
   const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`
   const data = await (await fetch(endpoint)).json()
-
+  return data.map.results((question: Question) => (
+    {
+      ...question,
+      answer: shuffleArray([...question.incorrect_answers, question.correct_answer])
+    }
+  ))
 }
